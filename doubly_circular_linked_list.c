8080be4982cbeef23c5fb2a77e6f083e314a946f
Barunwason <barunwason@gmail.com>
2024-09-22 22:56:33 +0530

    changes

--- doubly_circular_linked_list.c
+++ doubly_circular_linked_list.c
@@ -46,58 +46,58 @@ void create(struct Node *node)
     }
     else
     {
-        node->next = NULL;
+        head->prev = node;
+        node->next = head;
     }
 }
-void display()
+void display(struct Node *node)
 {
-    struct Node *temp = head;
-    while (temp != NULL)
+    while (node->next != head)
     {
-        printf("%d", temp->data);
-        temp = temp->next;
+        printf("%d", node->data);
+        node = node->next;
     }
+    printf("%d", node->data);
     printf("\n");
 }
-void reverse_display()
+bool is_element_in_list(int value)
 {
-    struct Node *temp = head;
-    while (temp->next != NULL)
+    struct Node *temp;
+    temp = head;
+    while (temp->next != head)
     {
+        if (temp->data == value)
+        {
+            return true;
+        }
         temp = temp->next;
     }
-
-    while (temp->prev != NULL)
+    if (temp->data == value)
     {
-        printf("%d", temp->data);
-        temp = temp->prev;
+        return true;
     }
-    printf("%d", temp->data);
-    printf("\n");
+    return false;
 }
 void insertion_at_begining()
 {
     int value;
     printf("Enter the number ");
     scanf("%d", &value);
-    struct Node *temp;
-    temp = (struct Node *)malloc(sizeof(struct Node));
-    if (head == NULL)
+    struct Node *temp = head;
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    while (temp->next != head)
     {
-        temp->data = value;
-        temp->next = head;
-        temp->prev = NULL;
-        head = temp;
+        printf("%d", temp->data);
+        temp = temp->next;
     }
-    else
-    {
-        temp->data = value;
-        temp->next = head;
-        head->prev = temp;
-        temp->prev = NULL;
-        head = temp;
-    }
+    ptr->data = value;
+    ptr->next = head;
+    head->prev = ptr;
+    temp->next = ptr;
+    ptr->prev = temp;
+    head = ptr;
 }
+
 void insertion_at_loc(int loc)
 {
     int value;
@@ -117,44 +117,29 @@ void insertion_at_loc(int loc)
     temp->next->prev = ptr;
     temp->next = ptr;
 }
+
 void insertion_at_the_end()
 {
     int value;
     printf("Enter the number ");
     scanf("%d", &value);
-    struct Node *temp;
-    struct Node *ptr;
-    ptr = (struct Node *)malloc(sizeof(struct Node));
-    ptr->data = value;
-    temp = head;
-    while (temp->next != NULL)
+    struct Node *temp = head;
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    while (temp->next != head)
     {
+        printf("%d", temp->data);
         temp = temp->next;
     }
+    ptr->data = value;
+    ptr->next = head;
+    head->prev = ptr;
     temp->next = ptr;
     ptr->prev = temp;
-    ptr->next = NULL;
 }
 void deletion_at_begining()
 {
     struct Node *temp;
-    if (head->next == NULL)
-    {
-        free(head);
-        head = NULL;
-    }
-    else
-    {
-        temp = head;
-        head = head->next;
-        head->prev = NULL;
-        free(temp);
-    }
-}
-
-void deletion_at_the_end()
-{
-    if (head->next == NULL)
+    if (head->next == head)
     {
         free(head);
         head = NULL;
@@ -163,21 +148,34 @@ void deletion_at_the_end()
     {
         struct Node *temp;
         temp = head;
-        while (temp->next != NULL)
-        {
-            temp = temp->next;
-        }
-        temp->prev->next = NULL;
+        head = head->next;
+        head->prev = temp->prev;
+        temp->prev->next = head;
+        free(temp);
+    }
+}
+void deletion_at_the_end()
+{
+    if (head->next == head)
+    {
+        free(head);
+        head = NULL;
+    }
+    else
+    {
+        struct Node *temp;
+        temp = head->prev;
+        head->prev = temp->prev;
+        temp->prev->next = head;
         free(temp);
     }
 }
 void deletion_at_loc(int loc)
 {
-    struct Node *temp;
-    temp = head;
+    struct Node *temp = head;
     if (head->data == loc)
     {
-        if (head->next == NULL)
+        if (head->next == head)
         {
             free(head);
             head = NULL;
@@ -194,7 +192,7 @@ void deletion_at_loc(int loc)
         {
             temp = temp->next;
         }
-        if (temp->next == NULL)
+        if (temp->next == head)
         {
             deletion_at_the_end();
         }
@@ -206,61 +204,49 @@ void deletion_at_loc(int loc)
         }
     }
 }
+
+void reverse()
+{
+    struct Node *temp = head;
+    struct Node *temp1 = NULL;
+    
+    do {
+        temp1 = temp->next;
+        temp->next = temp->prev;
+        temp->prev = temp1;
+        temp = temp1;  
+    } while (temp != head);
+
+    head = temp->prev;
+}
 void count_the_nodes()
 {
     int count = 0;
     struct Node *temp;
     temp = head;
-    while (temp != NULL)
+    while (temp->next != head)
     {
         count++;
         temp = temp->next;
     }
+    count++;
     printf("Total number of nodes are --> %d", count);
 }
-void reverse()
+void insert_when_head_is_null()
 {
-    struct Node *prev = NULL;
-    struct Node *current = head;
-    struct Node *next = NULL;
-
-    while (current != NULL)
-    {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
-    }
-
-    head = prev;
-    prev = NULL;
-    while (current != NULL)
-    {
-        prev = current->prev;
-        current->prev = next;
-        next = current;
-        current = prev;
-    }
-}
-bool is_element_in_list(int value)
-{
-    struct Node *temp = head;
-    while (temp != NULL)
-    {
-        if (temp->data == value)
-        {
-            return true;
-        }
-        temp = temp->next;
-    }
-    return false;
+    int value;
+    printf("Enter the value");
+    scanf("%d", &value);
+    head = (struct Node *)malloc(sizeof(struct Node));
+    head->data = value;
+    head->next = head;
+    head->prev = head;
 }
 int main()
 {
     head = (struct Node *)malloc(sizeof(struct Node));
-    head->prev = NULL;
     create(head);
-    display();
+    display(head);
     choice_operation();
     while (b == 0)
     {
@@ -272,15 +258,24 @@ int main()
             }
             else
             {
-                display();
+                display(head);
             }
             choice_operation();
         }
         else if (choice == 2)
         {
-            insertion_at_begining();
-            printf("Here is the updated list -->");
-            display();
+            if (head == NULL)
+            {
+                insert_when_head_is_null();
+                printf("Here is the updated list -->");
+                display(head);
+            }
+            else
+            {
+                insertion_at_begining();
+                printf("Here is the updated list -->");
+                display(head);
+            }
             choice_operation();
         }
         else if (choice == 3)
@@ -305,22 +300,22 @@ int main()
             {
                 printf("Element is not in the list\n");
             }
-            display();
+            display(head);
             choice_operation();
         }
         else if (choice == 4)
         {
             if (head == NULL)
             {
-                insertion_at_begining();
+                insert_when_head_is_null();
                 printf("Here is the updated list -->");
-                display();
+                display(head);
             }
             else
             {
                 insertion_at_the_end();
                 printf("Here is the updated list -->");
-                display();
+                display(head);
             }
             choice_operation();
         }
@@ -341,7 +336,7 @@ int main()
                 else
                 {
                     printf("Here is the updated list -->");
-                    display();
+                    display(head);
                 }
             }
             choice_operation();
@@ -394,7 +389,7 @@ int main()
                 }
                 else
                     printf("Here is the updated list -->");
-                display();
+                display(head);
             }
             choice_operation();
         }
@@ -417,7 +412,7 @@ int main()
             else
             {
                 reverse();
-                display();
+                display(head);
             }
 
             choice_operation();
